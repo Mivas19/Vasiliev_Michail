c7575fb328fa22c602ef29da1955681e282c0cf3
Michail <Deor1next@gmail.com>
2021-04-26 22:00:24 +0300

    git add -A

---
+++ __main__.py
@@ -0,0 +1,16 @@
+"""Главный файл программы, запускает приложение и настраивает журналирование."""
+
+import logging.config
+import os
+
+from app import create_app
+from modules.constants import LOGGING_CONFIG_FILE
+
+if __name__ == '__main__':
+    logging.config.fileConfig(LOGGING_CONFIG_FILE)
+
+    logging.info('Запуск приложения')
+
+    app = create_app()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
